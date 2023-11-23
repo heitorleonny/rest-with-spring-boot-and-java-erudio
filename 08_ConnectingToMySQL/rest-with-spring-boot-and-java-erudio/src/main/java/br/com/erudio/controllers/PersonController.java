7c752be844c641a1c411d8217c5e127dba4316a5
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-11-23 14:50:49 -0300

    add section 8

---
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,50 @@
+package br.com.erudio.controllers;
+
+import java.util.List;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+	@Autowired
+	private PersonServices service;
+	@GetMapping(value = "/{id}",
+	produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") Long id) {
+
+		return service.findById(id);
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+	consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person){
+
+		return service.create(person);
+	}
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person){
+
+		return service.update(person);
+	}
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+
+		return service.findAll();
+	}
+
+}
