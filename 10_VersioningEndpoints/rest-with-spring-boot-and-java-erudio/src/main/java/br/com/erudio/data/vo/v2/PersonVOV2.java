55101e1ca6b298f809e9bfe4c0082d91474636a6
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-11-27 19:02:37 -0300

    add section 10

---
+++ 10_VersioningEndpoints/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v2/PersonVOV2.java
@@ -0,0 +1,94 @@
+package br.com.erudio.data.vo.v2;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+
+public class PersonVOV2 implements Serializable {
+    private static  final long serialVersionUTD = 1L;
+
+
+    private Long id;
+
+
+    private String firstName;
+
+
+    private String lastName;
+
+
+    private String address;
+
+
+    private String gender;
+
+    private Date birthDay;
+
+    public PersonVOV2() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonVOV2 that = (PersonVOV2) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(address, that.address) && Objects.equals(gender, that.gender) && Objects.equals(birthDay, that.birthDay);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender, birthDay);
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
